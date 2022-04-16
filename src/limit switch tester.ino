9d39b341b9fef9acaed5fabb1a51390f5dff2dc5
dparker2112 <nlightn0@gmail.com>
2022-04-15 21:43:50 -0700

    now

--- src/limit switch tester.ino
+++ src/limit switch tester.ino
@@ -1,2 +1,15 @@
 #include <Arduino.h>
 
+byte falllocationPin = 26;
+
+byte fallPin = 31;
+
+byte ledPin = 13;
+
+void setup() { 
+
+    pinMode(falllocationPin, INPUT_PULLUP);
+    pinMode(ledPin, OUTPUT);
+    pinMode(fallPin, OUTPUT);
+
+}
